82bf3d0d5103c729dd25f8ab294ec15ef478501d
abhijay-agarwal <57358174+abhijay-agarwal@users.noreply.github.com>
2024-02-21 12:03:18 -0500

    add more specific routes for different query levels

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { getAllTiles, getById, getByAbstractSearchTerm, getOneTest, getExactTitleMatches } from "./sanityServer.js";
+import { getAllTiles, getById, getByAbstractSearchTerm, getOneTest, getPartialTitleMatches, getWholeTitleMatches, getPartialLabelMatches, getWholeLabelMatches } from "./sanityServer.js";
 import { setLiked, getLiked, delLiked, getAllLiked } from "./vercelServer.js";
 
 const server_port = 8080;
@@ -13,17 +13,29 @@ app.use(
   })
 );
 
+// server landing page
 app.get("/", (req, res) => {
-  res.json("Hello World!");
+  res.json("Hello World");
 });
+
+// useful routes for debugging
 app.get("/test", getOneTest);
 app.get("/all", getAllTiles);
-app.get("/id/:id", getById);
+
+// routes for querying Sanity
 app.get("/search/:searchText", getByAbstractSearchTerm);
-app.get("/title/:searchText", getExactTitleMatches);
+app.get("/search/exact/title/:searchText", getWholeTitleMatches);
+app.get("/search/title/:searchText", getPartialTitleMatches);
+app.get("/search/exact/label/:searchText", getWholeLabelMatches);
+app.get("/search/label/:searchText", getPartialLabelMatches);
+
+// routes for vercel KV store updates
 app.get("/get/:id", getLiked);
 app.put("/set/:id", setLiked);
 app.delete("/del/:id", delLiked);
+
+// route to display items
+app.get("/id/:id", getById);
 app.get("/liked", getAllLiked);
 
 app.listen(server_port, () => {
--- server/sanityServer.js
+++ server/sanityServer.js
@@ -2,7 +2,7 @@ import { createClient } from "@sanity/client";
 const SANITY_API_TOKEN = "sknYR53pYso8HNnC6DGdqIRNPsWPoA5ZqKNaQSoFqq7mvdTA8b4Z9dLCyv4kaYZXpyeA0Tgr6mdd0Rd90F7NZnTmG2uDxPOMOQRJw9lDrVrvTlJXJIJ1uNQTh0bXh0uv0bKWcNVmhRbwFInkarlYjpAvNsoV9ht1Ojtz2EUcwmCWh7Fl6ydC"
 
 // nltk stopwords list
-const stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
+const STOPWORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
 
 const config = {
   projectId: "0unlbb72",
@@ -57,10 +57,10 @@ const getAllTiles = async (req, res) => {
   }
 }
 
-const getExactTitleMatches = async (req, res) => {
+const getPartialTitleMatches = async (req, res) => {
   try {
     const { searchText } = req.params;
-    const keywords = searchText.split(" ").filter(word => !stopwords.includes(word.toLowerCase()));
+    const keywords = searchText.split(" ").filter(word => !STOPWORDS.includes(word.toLowerCase()));
 
     const filters = keywords.map(keyword => `(title match "${keyword}")`).join(" || ");
 
@@ -87,10 +87,10 @@ const getExactTitleMatches = async (req, res) => {
   }
 }
 
-const getWholePhraseMatches = async (req, res) => {
+const getPartialLabelMatches = async (req, res) => {
   try {
     const { searchText } = req.params;
-    const keywords = searchText.split(" ").filter(word => !stopwords.includes(word.toLowerCase()));
+    const keywords = searchText.split(" ").filter(word => !STOPWORDS.includes(word.toLowerCase()));
 
     const filters = keywords.map(keyword => `(labels[].value match "${keyword}")`).join(" || ");
 
@@ -117,11 +117,65 @@ const getWholePhraseMatches = async (req, res) => {
   }
 }
 
+const getWholeTitleMatches = async (req, res) => {
+  try {
+    const { searchText } = req.params;
+
+    const query = `
+      *[_type == "tile" &&
+      !(summary match "lorem*") &&
+      developing == false &&
+      !(_id in path("drafts.*")) &&
+      status == "published" &&
+      (title match "${searchText}")]
+      {
+        _id,
+        title,
+        summary,
+        "labels": labels[].value,
+        "image": sharingImage19_5x9Url
+      }`;
+    const data = await client.fetch(query);
+    res.json(data);
+  }
+  catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+}
+
+const getWholeLabelMatches = async (req, res) => {
+  try {
+    const { searchText } = req.params;
+
+    const query = `
+      *[_type == "tile" &&
+      !(summary match "lorem*") &&
+      developing == false &&
+      !(_id in path("drafts.*")) &&
+      status == "published" &&
+      (labels[].value match "${searchText}")]
+      {
+        _id,
+        title,
+        summary,
+        "labels": labels[].value,
+        "image": sharingImage19_5x9Url
+      }`;
+    const data = await client.fetch(query);
+    res.json(data);
+  }
+  catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+}
+
 const getByAbstractSearchTerm = async (req, res) => {
   try {
     const { searchText } = req.params;
     console.log(searchText);
-    const keywords = searchText.split(" ").filter(word => !stopwords.includes(word.toLowerCase()));
+    const keywords = searchText.split(" ").filter(word => !STOPWORDS.includes(word.toLowerCase()));
 
     const filters = keywords.map(keyword => `(title match "${keyword}*"  || labels[].value match "${keyword}")`).join(" || ");
 
@@ -149,4 +203,4 @@ const getByAbstractSearchTerm = async (req, res) => {
   }
 }
 
-export { client, getOneTest, getById, getAllTiles, getExactTitleMatches, getByAbstractSearchTerm };
+export { client, getOneTest, getById, getAllTiles, getPartialTitleMatches, getPartialLabelMatches, getWholeTitleMatches, getWholeLabelMatches, getByAbstractSearchTerm };
