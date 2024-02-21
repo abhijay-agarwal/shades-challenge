26cc6275b3d381dfe851391a18ee2ec19de753d4
abhijay-agarwal <57358174+abhijay-agarwal@users.noreply.github.com>
2024-02-20 21:58:39 -0500

    vercel setup

--- server/api/index.js
+++ server/api/index.js
@@ -1,24 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const config = require('./config');
-const sanity = require('./sanity');
-
-
-const app = express();
-app.use(cors({
-  origin: '*',
-}));
-
-
-app.get('/', (res) => {
-  res.json('Hello World!');
-});
-app.get('/count', sanity.getAllTiles);
-app.get('/:id', sanity.getById);
-app.get('/search/:searchTerm', sanity.getBySearchTerm);
-
-app.listen(config.server_port, () => {
-  console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
-});
+const app = require('../server');
 
 module.exports = app;
