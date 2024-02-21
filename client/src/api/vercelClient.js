c057650636986014d7873847e8f7154c25d5e05e
abhijay-agarwal <57358174+abhijay-agarwal@users.noreply.github.com>
2024-02-21 03:15:51 -0500

    change urls

--- client/src/api/vercelClient.js
+++ client/src/api/vercelClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const url = `https://shades-challenge.vercel.app`;
-const url = 'http://localhost:8080';
+const url = `https://shades-challenge.vercel.app`;
+// const url = 'http://localhost:8080';
 
 const setLike = async (id, like) => {
   console.log(id, like);
